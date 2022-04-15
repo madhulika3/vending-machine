8d75a51e0a607963025ec269755b871eb8fb4bc1
madhulika3 <madhulik@umich.edu>
2022-04-15 04:53:05 +0000

    Added my project

---
+++ demo/src/BeveragePreferenceService/BeveragePreferenceService.h
@@ -0,0 +1,25 @@
+#ifndef VENDING_MACHINE_MICROSERVICES_BEVERAGEPREFERENCESERVICE_H
+#define VENDING_MACHINE_MICROSERVICES_BEVERAGEPREFERENCESERVICE_H
+
+#include <iostream>
+#include <string>
+#include <regex>
+#include <future>
+
+#include "../../gen-cpp/OrderBeverageService.h"
+#include "../../gen-cpp/WeatherService.h"
+
+#include "../ClientPool.h"
+#include "../ThriftClient.h"
+#include "../logger.h"
+
+namespace vending_machine{
+class BeveragePreferenceHandler{
+getBeverage(BeverageType btype):
+	(0==rand()%2)? BeverageType:COLD::capuccino::latte::espresso:WARM:lemonade:iced tea::soda
+	
+}
+}
+
+
+
